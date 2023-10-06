cb31cf6709ca4fc8742d93fd1b5abc02c07effff
Mackie-S <masaki.shimadu@gmail.com>
2023-10-06 21:25:52 +0900

    feat: Day11終了

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
 
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :new, :create]
 end
